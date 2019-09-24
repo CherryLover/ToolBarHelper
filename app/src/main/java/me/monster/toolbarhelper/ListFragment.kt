43299f7896204e9a7ae5a8b60e047b264ca0c83c
jiangjiwei <jiangjiwei0322@outlook.com>
2019-09-24 13:14:52 +0800

    todo

--- app/src/main/java/me/monster/toolbarhelper/ListFragment.kt
+++ app/src/main/java/me/monster/toolbarhelper/ListFragment.kt
@@ -14,14 +14,30 @@ import me.monster.toolbarhelper.view.ToolView
  */
 class ListFragment : BaseFragment(), ToolClickListener {
     override fun onClick(what: Int) {
-        toast(
-            when (what) {
-                ToolView.navigation -> "返回"
-                ToolView.title -> "标题"
-                ToolView.menu -> "菜单按钮"
-                else -> "图标按钮"
-            }
-        )
+        when (what) {
+            ToolView.navigation -> navClick()
+            ToolView.title -> titleClick()
+            ToolView.menu -> menuClick()
+            else -> menuImgClick()
+        }
+    }
+
+    private fun navClick() {
+        // TODO: 2019-09-24 返回按钮的处理
+        toast("返回")
+    }
+
+    private fun titleClick() {
+        toast("标题")
+    }
+
+    private fun menuImgClick() {
+        toast("图标按钮")
+    }
+
+    private fun menuClick() {
+        // TODO: 2019-09-24 点击跳转到没有动态设置 ToolView 的页面
+        toast("菜单按钮")
     }
 
     override fun init() {
@@ -31,7 +47,7 @@ class ListFragment : BaseFragment(), ToolClickListener {
             toolHelper?.setTitle("测试列表")
             toolHelper?.setMenuImg(R.drawable.ic_send_black_24dp)
             toolHelper?.setMenu("详情")
-        }, 10 * 1000)
+        }, 3 * 1000)
     }
 
     override fun getLayoutId(): Int {
@@ -43,6 +59,6 @@ class ListFragment : BaseFragment(), ToolClickListener {
     }
 
     override fun getTitle(): String {
-        return "10 秒后设置标题"
+        return "3 秒后设置标题"
     }
 }
