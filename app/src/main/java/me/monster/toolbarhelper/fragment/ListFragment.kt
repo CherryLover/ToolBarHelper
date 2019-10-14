217164301f550904f2c1a839967ce57a0009d218
jiangjiwei <jiangjiwei0322@outlook.com>
2019-10-14 16:53:14 +0800

    feature 1. new Demo for ListFragment

--- app/src/main/java/me/monster/toolbarhelper/fragment/ListFragment.kt
+++ app/src/main/java/me/monster/toolbarhelper/fragment/ListFragment.kt
@@ -1,15 +1,12 @@
 package me.monster.toolbarhelper.fragment
 
 
-import android.os.Handler
-import android.os.Looper
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_list.*
 import me.monster.toolbarhelper.R
 import me.monster.toolbarhelper.base.BaseFragment
-
 import me.monster.toolbarhelper.toolview.ToolView
 import me.monster.toolbarhelper.toolview.nav.NavPop
 import me.monster.toolbarhelper.toolview.tools.ToolClickListener
@@ -28,40 +25,65 @@ class ListFragment : BaseFragment(), ToolClickListener {
     }
 
     private fun navClick() {
-        // TODO: 2019-09-24 返回按钮的处理
-        toast("返回")
+        toast("返回按钮的点击")
     }
 
     private fun titleClick() {
-        toast("标题")
+        toast("标题的点击")
     }
 
     private fun menuImgClick() {
-        toast("图标按钮")
+        toast("菜单图标按钮的点击")
     }
 
     private fun menuClick() {
-        // TODO: 2019-09-24 点击跳转到没有动态设置 ToolView 的页面
-        toolHelper?.setBackgroundRes(R.drawable.bg_color_gradient)
-        toast("设置背景色")
+        toast("菜单按钮的点击")
     }
 
     override fun init() {
         toolHelper?.setListener(this)
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            toolHelper?.setTitle("测试列表")
-            toolHelper?.setMenuImg(R.drawable.ic_send_black_24dp)
-            toolHelper?.setMenu("详情")
-            toolHelper?.setBgColor(android.R.color.holo_blue_light)
-        }, 3 * 1000)
-
-        toolHelper?.setPopProvider(NavPop)
 
         btn_list_entrance.setOnClickListener {
             Navigation.findNavController(it)
                 .navigate(R.id.action_listFragment_to_singleViewFragment)
         }
+
+        val menuList = mutableListOf<Map<String, String>>()
+        prepareListItem(menuList)
+        val simpleAdapter = android.widget.SimpleAdapter(
+            activity,
+            menuList,
+            android.R.layout.simple_list_item_1,
+            arrayOf("text"),
+            intArrayOf(android.R.id.text1)
+        )
+        lv_list_menu.adapter = simpleAdapter
+        lv_list_menu.setOnItemClickListener { parent, view, position, id ->
+            handleItemClick(position)
+        }
+    }
+
+    private fun handleItemClick(position: Int) {
+        when (position) {
+            0 -> toolHelper?.setTitle("点击更换标题")
+            1 -> toolHelper?.setMenu("详情")
+            2 -> toolHelper?.setMenuImg(R.drawable.ic_send_black_24dp)
+            3 -> toolHelper?.setBackgroundRes(R.drawable.bg_color_gradient)
+            4 -> toolHelper?.setPopProvider(NavPop)
+        }
+    }
+
+    private fun prepareListItem(menuList: MutableList<Map<String, String>>) {
+        val map = hashMapOf("text" to "更换标题")
+        val map1 = hashMapOf("text" to "更换右侧菜单")
+        val map2 = hashMapOf("text" to "显示菜单图片")
+        val map3 = hashMapOf("text" to "设置 ToolBar 背景（渐变色）")
+        val map4 = hashMapOf("text" to "托管 ToolBar 返回按钮点击")
+        menuList.add(map)
+        menuList.add(map1)
+        menuList.add(map2)
+        menuList.add(map3)
+        menuList.add(map4)
     }
 
     override fun getLayoutId(): Int {
@@ -69,10 +91,10 @@ class ListFragment : BaseFragment(), ToolClickListener {
     }
 
     override fun closeToolView(rootView: View): View? {
-        return rootView.findViewById(R.id.tv_list_label)
+        return rootView.findViewById(R.id.lv_list_menu)
     }
 
     override fun getTitle(): String {
-        return "3 秒后设置标题"
+        return "默认标题"
     }
 }
